5ffde243351a06a396fa9720a34e9e77c8974cfa
Abayomi Omosehin <abayomi.omosehin@venturegardengroup.com>
2021-02-23 20:55:39 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,39 @@
+
+
+def lesser_of_two_even(a,b):
+    if a%2 ==0 and b%2==0:
+        if a>b:
+            print(b)
+        else: print(a)
+    elif  a%2 !=0 or b%2 !=0:
+        if a>b:
+            print(a)
+        else: print(b)  
+
+lesser_of_two_even(4,8)
+
+def makes_twenty(n1,n2):
+    return n1+n2==20 or n1 == 20 or n2 ==20
+
+print(makes_twenty(21,20))
+
+def capitaliseText(text):
+    result = ''
+    for v,i in enumerate(text):
+        if v == 0 or v==3:
+            result+=  text[v].upper()
+        else:
+            result+= text[v]
+    
+    return result
+
+
+print(capitaliseText('macdonald'))
+
+def reverseString(text):
+
+    textResult =''
+    for j,i in enumerate(text):
+        print(text[j])
+
+reverseString('hello')
