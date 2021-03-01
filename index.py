7b154abbdc6f6ce891c7a3b09d8c331cfaf12b52
Abayomi Omosehin <abayomi.omosehin@venturegardengroup.com>
2021-03-01 15:31:59 +0100

    tripple character

--- index.py
+++ index.py
@@ -37,3 +37,23 @@ def reverseString(text):
     
 
 print(reverseString('hello adeoye'))
+
+#Given a list of ints, return True if the array contains a 3 next to a 3 somewhere
+def has_33(nums):
+    for i in range(0,len(nums)-1):
+        if nums[i] == 3 and nums[i + 1] == 3:
+            return True
+          
+    return False
+
+print ('result',has_33([3,2,1,3,1,3,3]))
+
+def paper_doll(word):
+    textin =''
+
+    for char in word:
+        textin += char*3
+    return textin
+
+print("triplword",paper_doll('hello'))
+
